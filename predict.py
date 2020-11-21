c61c4cf660c954a7535b370253b8aa5ad5f0a284
choi-seoghwan <74610959+choi-seoghwan@users.noreply.github.com>
2020-11-21 16:08:26 +0900

    Update predict.py
    합계 95미만, 175초과 제거

--- predict.py
+++ predict.py
@@ -8,6 +8,7 @@ Created on Fri Nov 20 17:02:01 2020
 import sqlite3
 import time
 
+# DB연결 현재깢지 나온 수(938)
 def untilNowNums():
     # DB 연결
     db_conn = sqlite3.connect("LOTTO.db", isolation_level=None)
@@ -41,6 +42,11 @@ def totalPossibleNums(until_now_nums):
 
     return until_nums
        
+# 합계 제거 ( 94 ~ 176 )
+def removeSumNums(possible_nums):
+    remove_sum_nums = [i for i in possible_nums if 94 < sum(i) < 176]
+    return remove_sum_nums
+
 # 시작
 start = time.time()
 # DB연결 및 현재(938)까지 나온 번호
@@ -49,4 +55,6 @@ until_now_nums = untilNowNums()
 # 나올 수 있는 번호 조합
 possible_nums = totalPossibleNums(until_now_nums)
 print("나올 수 있는 번호 조합 수 :", len(possible_nums),"걸린시간 : ", time.time() - start)
+possible_nums = removeSumNums(possible_nums)
+print("합계 95미만, 176초과 제거 수 :", len(possible_nums),"걸린시간 : ", time.time() - start)
 
